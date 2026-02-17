67a14a877f1258bcf315495d865206727d3f1e1f
bankutech <sagnikmitra1302@gmail.com>
2026-02-17 09:38:00 +0530

    Update palindrome checker logic

---
+++ palindromechecker/src/PalindromeCheckerApp.java
@@ -0,0 +1,12 @@
+
+public class PalindromeCheckerApp {
+    private static final String APP_NAME = "Palindrome Checker Management system";
+    private static final String APP_VERSION = "1.0";
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to the " + APP_NAME);
+        System.out.println("Version: " + APP_VERSION);
+
+        System.out.println("System initialized  successfully");
+    }
+}
---
+++ palindromechecker/src/PalindromeCheckerApp2.java
@@ -0,0 +1,9 @@
+public class PalindromeCheckerApp2{
+
+    public static void main(String[] args) {
+        System.out.println("print text");
+        for(int i = 0; i< input.length/2;i++{
+            System.out.println("Truc);
+        }
+    }
+}
