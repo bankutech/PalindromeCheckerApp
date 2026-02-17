c9a24d7864db2edc3c7a118f4baec41d19b19fe1
Bankutech <sagnikmitra@gmail.com>
2026-02-17 10:13:17 +0530

    UC1: Application Entry & Welcome Message

--- palindromechecker/src/PalindromeCheckerApp.java
+++ palindromechecker/src/PalindromeCheckerApp.java
@@ -2,7 +2,7 @@
 public class PalindromeCheckerApp {
     private static final String APP_NAME = "Palindrome Checker Management system";
     private static final String APP_VERSION = "1.0";
-    public static void main(String[] args) {
+    public static void main(String[] Args) {
 
         System.out.println("Welcome to the " + APP_NAME);
         System.out.println("Version: " + APP_VERSION);
