86231398f13073f255f6cb19d2d20610f20d1ff9
bankutech <sagnikmitra1302@gmail.com>
2026-02-17 09:43:23 +0530

    Update PalindromeCheckerApp2 logic

--- palindromechecker/src/PalindromeCheckerApp2.java
+++ palindromechecker/src/PalindromeCheckerApp2.java
@@ -2,8 +2,8 @@ public class PalindromeCheckerApp2{
 
     public static void main(String[] args) {
         System.out.println("print text");
-        for(int i = 0; i< input.length/2;i++{
-            System.out.println("Truc);
+        for(int i = 0; i< input.length/2;i++){
+            System.out.println("True");
         }
     }
 }
